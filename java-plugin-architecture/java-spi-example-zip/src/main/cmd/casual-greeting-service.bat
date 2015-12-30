a19056e8842eb16435c27fe6a15961378cd4a40d
René Link <rene.link@link-intersystems.com>
2015-12-30 17:54:38 +0100

    Added Java Plugin Architecture (SPI) and restructured maven modules

---
+++ java-plugin-architecture/java-spi-example-zip/src/main/cmd/casual-greeting-service.bat
@@ -0,0 +1,2 @@
+@echo off
+java -cp java-spi-1-SNAPSHOT.jar;java-spi-casual-greeting-service-1-SNAPSHOT.jar;java-spi-client-1-SNAPSHOT.jar com.link_intersystems.blog.plugin.client.GreetingApp %*
---
+++ java-plugin-architecture/java-spi-example-zip/src/main/cmd/formal-greeting-service.bat
@@ -0,0 +1,2 @@
+@echo off
+java -cp java-spi-1-SNAPSHOT.jar;java-spi-formal-greeting-service-1-SNAPSHOT.jar;java-spi-client-1-SNAPSHOT.jar com.link_intersystems.blog.plugin.client.GreetingApp %*
